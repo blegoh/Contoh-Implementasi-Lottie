2a8a3be555f49ae62f0cc4ea292c60a1995c0ee3
Yusuf Eka Sayogana <yusufblegoh@gmail.com>
2019-03-05 09:12:46 +0700

    first commit

---
+++ app/src/main/java/me/yusufeka/asubroto/MainActivity.java
@@ -0,0 +1,25 @@
+package me.yusufeka.asubroto;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView animationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        animationView = findViewById(R.id.animation_view);
+        animationView.setImageAssetsFolder("images");
+
+        animationView.playAnimation();
+
+//        animationView.setProgress(0.5f);
+    }
+}
